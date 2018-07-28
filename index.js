e06e8f04b9fc3cf7e0687a35da8d4ebdbd261f77
Ville Byman <villebyman@gmail.com>
2018-07-28 22:13:34 +0300

    persons added

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 const personsRouter = require('./controllers/persons')
+const persons = require('./db.json');
 app.use('/api/persons', personsRouter)
+
+//http://localhost:3001/info
+
+
+
+app.get('/info', (req, res) => {
+  const date = new Date();
+  const number = persons.persons.length;
+  res.send('<div>Puhelinluettelossa on '+number+' henkilön tiedot<\div>'+ date)
+  
+  
+})
 
 const PORT = 3001
 app.listen(PORT, () => {

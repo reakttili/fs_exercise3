1aae81647421f5e73e29353e93b9122e53b617ef
Ville Byman <villebyman@gmail.com>
2018-07-28 22:35:53 +0300

    delete works

--- controllers/persons.js
+++ controllers/persons.js
@@ -3,6 +3,12 @@ const personsdict = require('./db.json');
 
 module.exports = personsRouter
 
+personsRouter.delete('/:id', (request, response) => {
+  const filteredList = personsdict.persons.filter(item => Number(item.id) != Number(request.params.id))
+  personsdict.persons = filteredList;
+  response.json(personsdict.persons)  
+})
+
 // async not yet needed!
 personsRouter.get('/', async (request, response) => {
   

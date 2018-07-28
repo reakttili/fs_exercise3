1c9e41bcd48f9fbddf30bd61677472320cba010c
Ville Byman <villebyman@gmail.com>
2018-07-28 22:23:12 +0300

    get single

--- controllers/persons.js
+++ controllers/persons.js
@@ -1,10 +1,25 @@
 const personsRouter = require('express').Router()
-const persons = require('./db.json');
+const personsdict = require('./db.json');
 
 module.exports = personsRouter
 
 // async not yet needed!
 personsRouter.get('/', async (request, response) => {
   
-  response.json(persons)
+  response.json(personsdict.persons)
+})
+
+personsRouter.get('/:id', async (request, response) => {
+  try {
+    const persons = personsdict.persons  
+    const selected = persons[request.params.id-1];
+    if (selected) {
+      response.json(persons[request.params.id-1])  
+    } else {
+      response.status(400).json({ error: 'malformatted id' })  
+    }
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).json({ error: 'malformatted id' })
+  }
 })

8065d3f08fc622e377604706c21166876ba0d66a
Ville Byman <villebyman@gmail.com>
2018-07-31 22:23:01 +0300

    put works

--- controllers/persons.js
+++ controllers/persons.js
@@ -6,9 +6,37 @@ module.exports = personsRouter
 
 personsRouter.delete('/:id', (request, response) => {
   console.log("@DELETE")
-  const filteredList = personsdict.persons.filter(item => Number(item.id) != Number(request.params.id))
-  personsdict.persons = filteredList;
-  response.json(personsdict.persons)  
+  // WITHOUTDB
+  // const filteredList = personsdict.persons.filter(item => Number(item.id) != Number(request.params.id))
+  // personsdict.persons = filteredList;
+  // response.json(personsdict.persons)  
+
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
+
+personsRouter.put('/:id', (request, response) => {
+  console.log("@PUT")
+  const person = {
+    name: request.body.name,
+    number: request.body.number
+  }
+  Person
+  .findByIdAndUpdate(request.params.id, person, { new: true } )
+  .then(upPerson => {
+    response.json(Person.formatPerson(upPerson))
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(400).send({ error: 'malformatted id' })
+  })
+  
 })
 
 // async not yet needed!
@@ -29,26 +57,6 @@ personsRouter.post('/', async (request, response) => {
   console.log("@POST")
   const name = request.body.name;
   const number = request.body.number
-  // newPerson = {
-  //   "name": name,
-  //   "number": number,
-  //   "id": Math.random()*55555555
-  // }
-  // WITHOUT DB
-  // if (personsdict.persons.find(p => p.name === name)) {
-  //  return response.status(400).json({ error: 'name already added' })
-  // }
-   
-  // if (!name) {
-  //   return response.status(400).json({ error: 'name missing' })
-  // }
-  // if (!number) {
-  //   return response.status(400).json({ error: 'number missing' })
-  // }
-  // personsdict.persons = personsdict.persons.concat(newPerson)
-  // console.log(personsdict.persons)
-  //response.json(newPerson)
-
   const person =  new Person({
     "name": request.body.name,
     "number": request.body.number
@@ -63,6 +71,26 @@ personsRouter.post('/', async (request, response) => {
     .catch(error => {
       console.log(error)
     })
+
+  // WITHOUT DB
+  // newPerson = {
+  //   "name": name,
+  //   "number": number,
+  //   "id": Math.random()*55555555
+  // }
+  // if (personsdict.persons.find(p => p.name === name)) {
+  //  return response.status(400).json({ error: 'name already added' })
+  // }
+   
+  // if (!name) {
+  //   return response.status(400).json({ error: 'name missing' })
+  // }
+  // if (!number) {
+  //   return response.status(400).json({ error: 'number missing' })
+  // }
+  // personsdict.persons = personsdict.persons.concat(newPerson)
+  // console.log(personsdict.persons)
+  //response.json(newPerson)
 })
 
 personsRouter.get('/:id', async (request, response) => {
